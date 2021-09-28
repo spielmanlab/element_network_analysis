969cd692856981063a2b0d18bd60c679497a2f72
Stephanie J. Spielman <spielman@rowan.edu>
2021-09-28 10:10:07 -0400

    Updated by SJS with {performance}, utils.R, and five models

---
+++ utils.R
@@ -0,0 +1,78 @@
+
+# Obtain model formula and R^2 as HTML for plot subtitle
+get_model_info <- function(fitted_model, x_term)
+{
+  broom::tidy(fitted_model) -> coefficients
+  # Get coefficients into their own variables
+  coefficients %>%
+    filter(term == x_term) %>%
+    pull(estimate) -> slope
+  coefficients %>%
+    filter(term == "(Intercept)") %>%
+    pull(estimate) -> yint
+  # Are we subtracting or adding the y intercept?
+  if (yint >= 0)
+  {
+    sign <- " + "
+  } else {
+    sign <- " - "
+  }
+  
+  # Get the formula as a string
+  formula <- glue::glue(
+    "Y = ",
+    {round(slope, 3)}, "X", 
+    {sign}, {abs(round(yint, 3))}
+  )
+  
+  # Get the R^2
+  broom::glance(fitted_model) %>%
+    pull(r.squared) %>%
+    round(3) -> r2
+  
+  glue::glue(
+    "{formula} (*R<sup>2</sup> = {r2}*)"
+  )
+}
+
+
+
+# Function to build a network for a given element, considering only < 4.34 Ga
+parse_element_network <- function(element)
+{
+  # Build the full network. Returns a LIST with various important stuff.
+  dragon::initialize_network(elements_of_interest = element, 
+                             # age_range is inclusive and we don't want =4.34, so this is hopefully good enough.
+                             age_range = c(4.33, 0)) -> full_network
+  
+  # The important stuff is in `$edges` and `$locality_info` names
+  edges <- full_network$edges 
+  locality <- full_network$locality_info 
+  
+  # How many elements does element form minerals with?
+  edges %>%
+    dplyr::select(to) %>%
+    dplyr::distinct() %>%
+    dplyr::filter(to != element) %>% # not self
+    nrow() -> n_elements
+  
+  # How many minerals does element form?
+  edges %>%
+    dplyr::select(from) %>%
+    dplyr::distinct() %>%
+    nrow() -> n_minerals
+  
+  # At how many localities? 
+  locality %>%
+    # mindat_id column represents unique ID for localities
+    dplyr::select(mindat_id) %>%
+    dplyr::distinct() %>%
+    nrow() -> n_localities
+  
+  # whatever is written last in the function body gets returned
+  c("element" = element,
+    "n_elements" = n_elements,
+    "n_minerals" = n_minerals,
+    "n_localities" = n_localities)
+}
+
