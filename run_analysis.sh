005966378a76e9ccbc07426bed89f5f8d045e190
Stephanie <stephanie.spielman@gmail.com>
2022-03-20 17:40:43 -0400

    Update run_analysis.sh

--- run_analysis.sh
+++ run_analysis.sh
@@ -2,8 +2,8 @@
 
 
 # This bash script simply knits the Rmarkdown file, 
-# and copies the knitted HTML _and_ associated libs into `docs/`
-# so the Rmd can be rendered by gh-pages. Safer than htmlpreview thing.
+# and copies the knitted HTML into `docs/` so the Rmd
+# is rendered by gh-pages. Safer than htmlpreview thing.
 
 # Render
 Rscript -e "rmarkdown::render('analyze_element_networks.Rmd', clean = TRUE)"
